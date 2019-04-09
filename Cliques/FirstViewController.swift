802a1710d6e207ec3c4273aa73c4603db557f22d
Ethan Kusters <ethan@outlook.com>
2019-04-09 15:30:36 -0700

    Initial Commit

---
+++ Cliques/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Cliques
+//
+//  Created by Ethan Kusters on 4/9/19.
+//  Copyright © 2019 Ethan Kusters. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cliques/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Cliques
+//
+//  Created by Ethan Kusters on 4/9/19.
+//  Copyright © 2019 Ethan Kusters. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
