ad4e23d9d2ef1747843bf4da2f3a0fd681a405fc
Ethan Kusters <ethan@outlook.com>
2019-05-10 14:36:13 -0700

    Completed initial login feature

---
+++ Cliques/FirebaseLoginController.swift
@@ -0,0 +1,43 @@
+//
+//  FirebaseLoginController.swift
+//  Cliques
+//
+//  Created by Ethan Kusters on 5/9/19.
+//  Copyright © 2019 Ethan Kusters. All rights reserved.
+//
+
+import FirebaseAuth
+import Firebase
+
+class FirebaseLoginController {
+    func verifyPhoneNumber(phoneNumber: String, verificationRequestSuccess:@escaping (Error?)->()) {
+        let number = "+1" + phoneNumber
+        
+        PhoneAuthProvider.provider().verifyPhoneNumber(number, uiDelegate: nil) { (verificationID, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription);
+            } else {
+                UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
+            }
+            
+            verificationRequestSuccess(error)
+        }
+    }
+    
+    func signInWithVerificationCode(verificationCode: String, signInSuccess:@escaping(Error?, AuthDataResult?)->()) {
+        guard let verificationID = UserDefaults.standard.string(forKey: "authVerificationID") else {
+            signInSuccess(nil, nil)
+            return
+        }
+        
+        let credential = PhoneAuthProvider.provider().credential(withVerificationID: verificationID, verificationCode: verificationCode)
+        
+        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+            }
+            
+            signInSuccess(error, authResult)
+        }
+    }
+}
