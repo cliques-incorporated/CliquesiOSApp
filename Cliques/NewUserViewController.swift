6808834ccceb3fe214ced010077e6a478d367b11
Ethan Kusters <ethan@outlook.com>
2019-05-11 11:07:51 -0700

    Finalized profile creation screen

--- Cliques/NewUserViewController.swift
+++ Cliques/NewUserViewController.swift
@@ -13,13 +13,28 @@ import Photos
 class NewUserViewController: UIViewController {
     @IBOutlet weak var FirstNameTextField: UITextField!
     @IBOutlet weak var LastNameTextField: UITextField!
+    @IBOutlet weak var BioTextField: UITextField!
     @IBOutlet weak var SelectProfileImageButton: UIButton!
-    let imageRequestController = ImageRequestController()
+    @IBOutlet weak var ProfileImageView: UIImageView!
     
+    private var imageSelected = false
+    private let imageRequestController = ImageRequestController()
+    private let allFieldsRequiredAlert = UIAlertController(title: "We're missing something...", message: "All fields are required!", preferredStyle: .alert)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        allFieldsRequiredAlert.addAction((UIAlertAction(title: "Got It", style: .cancel, handler: nil)))
+        
+        let profileImageBorder = CALayer()
+        profileImageBorder.borderColor = UIColor.black.cgColor
+        let profileImageBorderWidth:CGFloat = 15.0
+        profileImageBorder.borderWidth = profileImageBorderWidth
+        profileImageBorder.cornerRadius = 8.0
+        profileImageBorder.frame = CGRect.init(x: -profileImageBorderWidth + 5, y: -profileImageBorderWidth + 5, width: ProfileImageView.frame.size.width + 2 * profileImageBorderWidth - 10, height: ProfileImageView.frame.size.height + 2 * profileImageBorderWidth - 10)
+        
+        ProfileImageView.layer.addSublayer(profileImageBorder)
+        ProfileImageView.layer.masksToBounds = false
     }
     
     @IBAction func FirstNameReturnButtonPressed(_ sender: Any) {
@@ -27,7 +42,11 @@ class NewUserViewController: UIViewController {
     }
     
     @IBAction func LastNameReturnButtonPressed(_ sender: Any) {
-        SelectProfileImageButton.becomeFirstResponder()
+        BioTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func BioReturnButtonPressed(_ sender: Any) {
+        BioTextField.resignFirstResponder()
     }
     
     @IBAction func SelectProfileImagePressed(_ sender: Any) {
@@ -35,9 +54,19 @@ class NewUserViewController: UIViewController {
     }
     
     private func imageSelected(image: UIImage?) {
-        
+        guard let image = image else { return }
+        ProfileImageView.image = image
+        imageSelected = true
     }
     
+    @IBAction func LetsGoButtonPressed(_ sender: Any) {
+        guard !(FirstNameTextField.text?.isEmpty ?? true), !(LastNameTextField.text?.isEmpty ?? true), !(BioTextField.text?.isEmpty ?? true), imageSelected else {
+            present(allFieldsRequiredAlert, animated: true, completion: nil)
+            return
+        }
+        
+        
+    }
     
     @IBAction func TextFieldEditingBegan(_ sender: Any) {
         if let textField = sender as? UITextField {
