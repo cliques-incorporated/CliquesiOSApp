ead70da3252c1e828ef9501e6e188f40f296e16e
Ethan Kusters <ethan@outlook.com>
2019-05-17 14:02:19 -0700

    Completed UI

--- Cliques/View Controllers/ConnectionsViewController.swift
+++ Cliques/View Controllers/ConnectionsViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ConnectionsViewController: UICollectionViewController {
-
+    private var searchController: UISearchController!
+    private var searchResultsController = AddConnectionSearchViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.definesPresentationContext = true
+        searchController = UISearchController(searchResultsController: searchResultsController)
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.placeholder = "Add a Connection"
+        searchController.searchResultsUpdater = searchResultsController
     }
 
     @IBAction func SearchButtonPressed(_ sender: Any) {
