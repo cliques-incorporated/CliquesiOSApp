29a0702570732b9213c85e17a220bf0eb63d8e36
Ethan Kusters <ethan@outlook.com>
2019-06-11 14:51:16 -0700

    Refactored to MVC structure

---
+++ Cliques/View Controllers/ConnectionsViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ConnectionsTableViewController.swift
+//  Cliques
+//
+//  Created by Brian Phan on 6/1/19.
+//  Copyright © 2019 Ethan Kusters. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseUI
+
+class ConnectionsViewController: UITableViewController, UISearchResultsUpdating{
+    @IBOutlet var UserConnectionTableView: UITableView!
+    var searchController = UISearchController()
+    private var userModel: UserModelSingleton!
+    private var filteredConnections = [Connection]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userModel = UserModelSingleton.GetInstance()
+        
+        searchController = ({
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.sizeToFit()
+            tableView.tableHeaderView = controller.searchBar
+            
+            return controller
+            
+        })()
+        
+        
+        userModel.updateConnections(completion: connectionsUpdated)
+    }
+    
+    private func connectionsUpdated(success: Bool) {
+        guard success else { return }
+        
+        UserConnectionTableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive {
+            return filteredConnections.count
+        } else {
+            return userModel.getConnections().count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserConnectionCell", for: indexPath) as? ConnectionTableViewCell else {
+            fatalError("The dequeued cell is not an instance of UserConnectionCell.")
+        }
+
+        var currentConnection: Connection
+        if(searchController.isActive){
+            currentConnection = filteredConnections[indexPath.row]
+        } else {
+            currentConnection = userModel.getConnections()[indexPath.row]
+        }
+        
+        cell.name.text = (currentConnection.profile.first ?? "") + " " + (currentConnection.profile.last ?? "")
+        cell.profileImage.sd_setImage(with: currentConnection.profileImage)
+        
+        if let clique = currentConnection.clique {
+            cell.name.textColor = CliqueUtility.GetCliqueColor(clique: clique)
+        } else {
+            cell.name.textColor = .black
+        }
+        
+        return cell
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredConnections = userModel.getPossibleConnections()
+        
+        if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+            filteredConnections = userModel.getPossibleConnections().filter{($0.profile.first ?? "").contains(searchText)}
+        }
+        
+        tableView.reloadData()
+    }
+}
