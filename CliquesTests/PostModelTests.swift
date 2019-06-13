ce699816c57a0f3617b76b0431cebf100a955344
Ethan Kusters <ethan@outlook.com>
2019-06-13 15:21:51 -0700

    Added feed model tests

--- CliquesTests/PostModelTests.swift
+++ CliquesTests/PostModelTests.swift
@@ -2,8 +2,39 @@
 //  PostModelTests.swift
 //  CliquesTests
 //
-//  Created by Brian Phan on 6/13/19.
+//  Created by Ethan Kusters on 6/13/19.
 //  Copyright © 2019 Ethan Kusters. All rights reserved.
 //
 
-import Foundation
+import XCTest
+
+class PostModelTests: XCTestCase {
+
+    func testPost_1(){
+        let mockFirestore = MockFirestoreController()
+        let mockFirebaseStorage = MockFirebaseStorageController()
+        let image = UIImage()
+        mockFirebaseStorage.mockUploadPostImageResp = true
+        
+        let PM = PostModel.init(image: image, caption: "", publicClique: false, friendsClique: false, closeFriendsClique: false, familyClique: false, firestoreController: mockFirestore, firebaseStorageController: mockFirebaseStorage)
+        
+        PM.upload(){
+            success in
+            XCTAssertTrue(success)
+        }
+    }
+    
+    func testPost_2(){
+        let mockFirestore = MockFirestoreController()
+        let mockFirebaseStorage = MockFirebaseStorageController()
+        let image = UIImage()
+        mockFirebaseStorage.mockUploadPostImageResp = false
+        let PM_one = PostModel.init(image: image, caption: "", publicClique: false, friendsClique: false, closeFriendsClique: false, familyClique: false, firestoreController: mockFirestore, firebaseStorageController: mockFirebaseStorage)
+        
+        PM_one.upload(){
+            success in
+            XCTAssertFalse(success)
+        }
+    }
+
+}
